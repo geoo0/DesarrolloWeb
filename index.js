d9060fc99cc2c01f2008a049aeccc262318ab41c
Geo <88101173+geoo0@users.noreply.github.com>
2025-08-31 15:36:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 3000;
 const pool = new Pool({
   host: "dpg-d2qba16r433s73e4mmh0-a.oregon-postgres.render.com",
   port: 5432,
-  user: "base_de_datos_m5si",      // <- Render te da un usuario
-  password: "base_de_datos_m5si_user", // <- Render te da un password
-  database: "base_de_datos_m5si_user",   // <- Render te da el nombre de la BD
-  ssl: { rejectUnauthorized: false }, // Render requiere SSL
+  user: "base_de_datos_m5si",    
+  password: "base_de_datos_m5si_user", 
+  database: "base_de_datos_m5si_user",  
+  ssl: { rejectUnauthorized: false }, 
 });
 
-// Ruta principal que hace SELECT * FROM usuarios
+
 app.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM usuarios;");
@@ -26,7 +26,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
